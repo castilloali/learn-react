9ccf01f97319dd2e53d3664d62aee847247d9604
cass <alicastilo1097@gmail.com>
2023-05-18 19:31:07 +0300

    learn

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+
+
+
+
 import "./style.css"
 
 export default function News() {
@@ -10,6 +14,33 @@ export default function News() {
         </div>
         <button className="btn">Add</button>
       </form>
+
+      <div className="hold">
+        <h2 className="header">Todo list</h2>
+        <ul className="list">
+          <li>
+            <label>
+              <input type="checkbox" name="" id="" />
+              item 1
+            </label>
+            <button className=" btn-danger">Danger</button>
+          </li>
+          <li>
+            <label>
+              <input type="checkbox" name="" id="" />
+              item 1
+            </label>
+            <button className=" btn-danger">Danger</button>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
+
+
+
+let name = "Fff"
+
+
+console.log(name)
